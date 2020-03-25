d4e192b0ac795bd904cdce31ff51a813dd53f604
shruti-shrz <54719029+shruti-shrz@users.noreply.github.com>
2020-03-25 13:28:43 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -34,8 +34,7 @@ freezer = Freezer(app)
 # def show() -> 'html':
 #     return render_template('./index.html')
 
-if __name__ == "__main__":
-	if len(sys.argv) > 1 and sys.argv[1] == "build":
+if len(sys.argv) > 1 and sys.argv[1] == "build":
 		freezer.freeze()
-	else:
+else:
 		app.run(port=8000)
