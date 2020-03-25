cfb96fa223a85ca5d15d45acfd4d2d046629412e
shruti-shrz <54719029+shruti-shrz@users.noreply.github.com>
2020-03-25 13:24:04 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def show() -> 'html':
     return render_template('./index.html')
-
+freezer = Freezer(app)
 @app.route('/p2.html')
 def show2() -> 'html':
     return render_template('./p2.html')
+freezer = Freezer(app)
 @app.route('/p3.html')
 def show3() -> 'html':
     return render_template('./p3.html')
+freezer = Freezer(app)
 @app.route('/p4.html')
 def show4() -> 'html':
     return render_template('./p4.html')
+freezer = Freezer(app)
 @app.route('/p5.html')
 def show5() -> 'html':
     return render_template('./p5.html')
+freezer = Freezer(app)
 @app.route('/p6.html')
 def show6() -> 'html':
     return render_template('./p6.html')
